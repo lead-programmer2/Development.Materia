33abb1f7042097f7561b9c271f885808603510b5
seph <rspm2@fms.com.ph>
2012-09-05 09:23:02 +0800

    Added a class to automate DSN connection.

---
+++ Development.Materia/Database/Dsn.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace Development.Materia.Database
+{
+    /// <summary>
+    /// Class to create a DSN data access programatically.
+    /// </summary>
+    public class Dsn
+    {
+        [DllImport("odbccp32.dll")]
+        private static extern int SQLConfigDataSource(int hwnd, int request, string driver, string attributes);
+
+        /// <summary>
+        /// Creates the Dsn with the specified database driver and database connection attributes.
+        /// </summary>
+        /// <param name="driver"></param>
+        /// <param name="attributes"></param>
+        /// <returns>True if DSN has been added, otherwise false.</returns>
+        public static bool Create(string driver, string attributes)
+        {
+            int _result = SQLConfigDataSource(0, 4, driver, attributes);
+            if (_result == 1) return true;
+            else return false;
+        }
+
+    }
+}
