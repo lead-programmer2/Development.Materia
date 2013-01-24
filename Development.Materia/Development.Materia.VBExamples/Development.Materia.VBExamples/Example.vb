d6ffb4dc01ed90702dd8e8cef576efc9e9f2f951
seph <rspm2@fms.com.ph>
2013-01-24 14:18:45 +0400

    Updated xml documentation to most of the methods. Added code samples.

---
+++ Development.Materia/Development.Materia.VBExamples/Development.Materia.VBExamples/Example.vb
@@ -0,0 +1,24 @@
+﻿Imports Development.Materia
+
+Public Class Example
+
+    Private Sub AmountToWordsConverter01()
+        ' #Region AmountToWordsConverter 01
+        Dim _words As String = AmountToWordsConverter.AmountToWords(20.05)
+        ' #End Region
+    End Sub
+
+    Private Sub AmountToWordsConverter02()
+        ' #Region AmountToWordsConverter 02
+        Dim _words As String = AmountToWordsConverter.AmountToWords(20.05, "USD")
+        ' #End Region
+    End Sub
+
+    Private Sub AmountToWordsConverter03()
+        ' #Region AmountToWordsConverter 03
+        Dim _converter As New AmountToWordsConverter(20.05, "USD")
+        Dim _words As String = _converter.ToString
+        ' #End Region
+    End Sub
+
+End Class
---
+++ Development.Materia/Examples/Example.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Development.Materia
+{
+    class Example
+    {
+
+        private void AmountToWordsConverter01()
+        {
+            #region "AmountToWordsConverter 01"
+            string _words = AmountToWordsConverter.AmountToWords(20.05);
+            #endregion
+        }
+
+        private void AmountToWordsConverter02()
+        {
+            #region "AmountToWordsConverter 02"
+            string _words = AmountToWordsConverter.AmountToWords(20.05, "USD");
+            #endregion
+        }
+
+        private void AmountToWordsConverter03()
+        {
+            #region "AmountToWordsConverter 03"
+            AmountToWordsConverter _converter = new AmountToWordsConverter(20.05, "USD");
+            string _words = _converter.ToString();
+            #endregion
+        }
+    }
+}
